888bfd0e91da71697ccb6a3e348e850c854faf54
nikhilshah85 <nikhilshah.net@hotmail.com>
2026-09-01 01:50:54 +0530

    Day 1

---
+++ Day1 - Environment Setup/firstapp/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("This is my very first project in .Net core, and its easy so far");
+
+string myName = "Nikhil";
+string myHobbie = "Writing";
+
+Console.WriteLine(myName);
+Console.WriteLine(myHobbie);
+
+System.Console.WriteLine("Please enter your name");
+string guestName = Console.ReadLine();
+System.Console.WriteLine("Welcome " + guestName);
+
+
+
