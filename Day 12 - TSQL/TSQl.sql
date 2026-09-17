a1ba76a6eafda36767c34ece2a09391a62fac4c3
nikhilshah85 <nikhilshah.net@hotmail.com>
2026-09-17 21:20:44 +0530

    Day 12

--- Day 12 - TSQL/TSQl.sql
+++ Day 12 - TSQL/TSQl.sql
@@ -67,9 +67,113 @@ select dbo.calculate(10,5,'+') as Calculate
 
 	select dbo.CTC('Masters',6)-- this should return the annual salary
 
+=============================================================================================================
+
+
+	alter procedure addDept
+	( 
+	  @deptName varchar(20),
+	  @deptLocation varchar(20)
+	  )
+	  AS
+	  begin
+		--we can format this inputs like
+		declare @formatedLocation varchar(20) = upper(@deptLocation)
+		insert into deptInfo values(@deptName,@formatedLocation)
+	  end
+
+	  exec addDept 'Consultancy','Texas'
+	  select * from deptInfo
+
+
+
+
+	  create table CredInfo
+		(
+			userName varchar(20),
+			pwd varchar(20)
+
+			constraint pk_uName primary key(userName),
+			constraint ck_pwdLen check(len(pwd) > 5)
+		)
+
+		insert into CredInfo values('userOne','Pass@4321')
+		insert into CredInfo values('secondUser','Pass@4532')
+		insert into CredInfo values('user_three','Pass@6784')
+		insert into CredInfo values('forth_user','Pass@2343')
+		insert into CredInfo values('I_am_fifth','Pass@67565')
+
+		create procedure credSP
+		(
+			@uName varchar(20),
+			@pwd varchar(20),
+			@newPwd varchar(20) = '',
+			@action varchar(10),
+			@result varchar(40) output
+		)
+		as
+		begin
+			if(@action = 'Login')
+			begin
+				declare  @loginresult int = (select count(*) from CredInfo where userName = @uName and pwd = @pwd)
+				if (@loginresult = 1)
+				begin
+					set @result = 'Login Successful'
+				end
+				else
+				begin
+					set @result = 'Invalid Credentials'
+
+				end
+			end
+
+			if(@action = 'NewUser')
+			begin
+				insert into credInfo values(@uName, @pwd)
+				set @result = 'User Added Successfully'
+			end
+
+			if(@action = 'ChangePwd')
+			begin
+				update credinfo set pwd = @newPwd where userName = @uName
+				set @result = 'Password Changed'
+			end
+
+			if(@action = 'DeleteAcc')
+			begin
+				delete from credInfo where userName = @uName
+				set @result = 'Account Deleted'
+			end
+
+		end
+
+		select * from credInfo
+
+
+		declare @res varchar(20)
+		exec credSP 'forth_user','','','DeleteAcc', @res output
+		print @res
+
+
+		select * from credInfo
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+		select * from credInfo where userName = 'userOne' and pwd = 'Pass@4321'
+
+		select count(*) from credInfo where userName = 'userOne' and pwd = 'sdfd@4321'
