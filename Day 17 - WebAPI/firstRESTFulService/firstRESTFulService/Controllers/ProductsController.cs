985e88e6aa94fdc45b6571d45d8dcab3d6b1025a
nikhilshah85 <nikhilshah.net@hotmail.com>
2026-09-24 00:36:17 +0530

    Day 17 Web API'

---
+++ Day 17 - WebAPI/firstRESTFulService/firstRESTFulService/Controllers/ProductsController.cs
@@ -0,0 +1,138 @@
+﻿using firstRESTFulService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace firstRESTFulService.Controllers
+{
+    //[] are called as attributes, attiibutes are information given to compiler to treat the code in a certain way
+    [Route("api/[controller]")] //to access the methods of this controller
+    [ApiController] //makes it a controller - 
+    public class ProductsController : ControllerBase
+    {
+
+        // we write action methods here
+        //methods will return Http Status codes
+        //methods decorated with Http Verbs ,
+        //[HTTPGET] for select or get data, image, video, user can get anything from server
+        //[HTTPPOST] for create(insert) data, image, video, user can get anything from server
+        //[HTTPPUT] for update(insert) data, image, video, user can get anything from server
+        //[HTTPDELETE] for delete(insert) data, image, video, user can get anything from server
+        //[HTTPPATCH] for update(insert) data, image, video, user can get anything from server
+
+
+        ProductsModel model = new ProductsModel(); // this is bad and worst code, we will use Dependency injection here
+
+        #region Get Methods
+
+        [HttpGet]
+        [Route("allproducts")]
+        public IActionResult GetAllProducts()
+        {
+            return Ok(model.GetALlProducts());
+
+        }
+
+        [HttpGet]
+        [Route("searchbyid/{id}")]
+        public IActionResult SearchProductById(int id)
+        {
+            try
+            {
+                return Ok(model.GetProductByID(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("searchbycategory/{category}")]
+        public IActionResult SearchByCategory(string category) {  
+            try
+            {
+                return Ok(model.GetProductsByCategory(category));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("totalproducts")]
+        public IActionResult GetTotalProducts() 
+        { 
+            return Ok(model.TotalProducts());
+        }
+        #endregion
+
+
+        [HttpPost]
+        [Route("addnewproduct")]
+        public IActionResult AddNewProduct(ProductsModel newProduct)
+        {
+            try
+            {
+               var addResult =  model.AddProduct(newProduct);
+                return Created("",addResult);
+            }
+            catch (Exception es)
+            {
+
+                return BadRequest(es.Message);
+            }      
+               
+        }
+
+        [HttpDelete]
+        [Route("deleteproduct/{id}")]
+        public IActionResult DeleteProduct(int id)
+        { 
+            try
+            {
+                var deleteResult = model.DeleteProduct(id);
+                return Ok(deleteResult);
+
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("updateproduct")]
+        public IActionResult EditProduct(ProductsModel product_to_update)
+        {
+            try
+            {
+                var updateResult = model.UpdateProduct(product_to_update);
+                return Ok(updateResult);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+        }
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Day 17 - WebAPI/firstRESTFulService/firstRESTFulService/Models/ProductsModel.cs
@@ -0,0 +1,139 @@
+﻿namespace firstRESTFulService.Models
+{
+    public class ProductsModel
+    {
+
+        #region Properties
+        public int ProductID { get; set; }
+        public string ProductName { get; set; } = "";
+
+        public string CategoryName { get; set; } = "";
+        public string Description { get; set; } = "";
+        public int Price { get; set; }
+        #endregion
+
+        #region Seed Data
+        //this is a hardcoded list, but in real it can be from database or excel file or any other source
+        static List<ProductsModel> productsList = new List<ProductsModel>()
+        {
+            new ProductsModel() { ProductID = 1, ProductName = "Pepsi", CategoryName = "Cold-Drink", Description = "A cold drink to beat your thirst and add energy", Price = 2},
+            new ProductsModel() { ProductID = 2, ProductName = "Lays", CategoryName = "Junk food", Description = "A yummy and tasty snack for quick filler to kill your hunger", Price = 4 },
+            new ProductsModel() { ProductID = 3, ProductName = "Ferrero", CategoryName = "Choclates", Description = "Sweet and delicious choclates for your pocket", Price = 6 },
+            new ProductsModel() { ProductID = 4, ProductName = "Nike", CategoryName = "Shoes", Description = "Just do wrong the right way", Price = 20 },
+            new ProductsModel() { ProductID = 5, ProductName = "IPhone", CategoryName = "Phone", Description = "Nothing new comes here", Price = 3000 },
+            new ProductsModel() { ProductID = 6, ProductName = "Adidas", CategoryName = "Shoes", Description = "Just do wrong the right way", Price = 20 },
+            new ProductsModel() { ProductID = 7, ProductName = "Puma", CategoryName = "Shoes", Description = "something new", Price = 6000 },
+
+
+
+        };
+
+        #endregion
+
+        #region CRUD Methods
+
+             #region Get Methods
+        public List<ProductsModel> GetALlProducts()
+        { 
+            return productsList;
+        }
+
+        public ProductsModel GetProductByID(int id)
+        {
+            //this is LINQ
+            //var p = from pr in productsList
+            //        where pr.ProductID == id
+            //        select pr;
+
+            //Lambda
+
+            var p2 = productsList.SingleOrDefault(pr => pr.ProductID == id); //returns a null if product is not present
+
+            if(p2 != null)
+            {
+
+                return p2;
+            }
+            else
+            {
+                throw new Exception("Product Not found in system");
+            }
+        }
+
+        public int TotalProducts()
+        {
+            return productsList.Count;
+        }
+
+        public List<ProductsModel> GetProductsByCategory(string category)
+        { 
+               var p = from pr in productsList
+                    where pr.CategoryName == category
+                    select pr;
+                
+
+            return p.ToList();
+        }
+        #endregion
+
+             #region Add, delete and Update Methods
+        public string AddProduct(ProductsModel product)
+        {
+
+            //we cando validation here
+
+            if (product.Price < 2)
+            {
+                throw new Exception("Price should be more than 2");
+            }
+            else
+            {
+                productsList.Add(product);
+
+                return "Product added successfully";
+            }
+        }
+
+        public string DeleteProduct(int id)
+        {
+            var p = productsList.Where(pr => pr.ProductID == id).Single();
+
+            if (p != null)
+            {
+                productsList.Remove(p);
+                return "Product deleted successfully";
+            }
+            else
+            {
+                throw new Exception("Product not found in system"); 
+            }
+        }
+
+        public string UpdateProduct(ProductsModel product)
+        {
+            var p = productsList.Where(pr => pr.ProductID == product.ProductID).Single();
+
+            if (p != null)
+            {
+                p.ProductName = product.ProductName;
+                p.CategoryName = product.CategoryName;
+                p.Description = product.Description;
+                p.Price = product.Price;
+
+                return "Product updated successfully";
+            }
+            else
+            {
+                throw new Exception("Invalid product Id");
+            }
+          
+        }
+
+        #endregion
+
+
+       #endregion
+
+
+    }
+}
